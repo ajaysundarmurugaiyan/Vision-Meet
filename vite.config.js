6303d752f6c02d6c87b14f8ff8c49252cb3953c6
AJAY SUNDAR <139756597+ajaysundarmurugaiyan@users.noreply.github.com>
2025-11-29 11:11:45 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,7 @@ export default defineConfig(({ mode }) => {
 
   return {
     define: {
-      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+      'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
     },
     plugins: [
       basicSsl()
